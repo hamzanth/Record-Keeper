3b29663b877eadeb4ff9ec4b4686dce20ea944f2
hamzanth <irekpitaanthony10@gmail.com>
2023-09-25 02:38:05 -0700

    Made changes to the admin dashboard

--- src/components/adminDashboard.jsx
+++ src/components/adminDashboard.jsx
@@ -5,7 +5,7 @@ const AdminDashBoard = () => {
   return (
     <>
       <h3 style={{color: "black"}}>This is the Admin Dashboard</h3>
-      <h2 style={{color: "red"}}>All the records are here</h2>
+      <h2 style={{color: "red"}}>All the records are heres</h2>
     </>
   )
 }
