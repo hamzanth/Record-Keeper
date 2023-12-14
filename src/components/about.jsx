4a51f881c9d1545f6de6084cb7f508d34a7fa925
hamzanth <irekpitaanthony10@gmail.com>
2023-12-14 09:19:49 -0800

    worked on the about and the home page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,7 +9,14 @@ const About = () => {
             <NavBar />
             <div className={styles.aboutContainer}>
                 <h1 style={{textAlign: "center"}}>Al Muwafaq Stores</h1>
-                <p style={{textAlign: "center"}}>We provide the best services world wide</p>
+                <div style={{width: "50%", margin: "auto"}}>
+                    <p style={{textAlign: "center", fontSize: "19px"}}>
+                        We offer good and quality services with care as the our motto implies. We try as much as we can to
+                        to give utmost priority to our customers and ensure that their needs are always met in the sense that
+                        whatever they request for is usually made available for them in real time. <br/>
+                        Also we try as much as can to make sure that honesty, transparency, quality is part of our recognition
+                    </p>
+                </div>
             </div>
         </>
     )
